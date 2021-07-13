40ff2a4f15aa9e89b8ad94933376b23c7dac25d6
Carlo Angel Luján García <carlouxljlj@gmail.com>
2021-07-13 16:05:28 -0500

    User can see a google map inside the explore fragment

--- Sailor/app/src/main/java/com/carlolj/sailor/ui/explore/ExploreFragment.java
+++ Sailor/app/src/main/java/com/carlolj/sailor/ui/explore/ExploreFragment.java
@@ -10,7 +10,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.carlolj.sailor.BuildConfig;
+import com.carlolj.sailor.R;
 import com.carlolj.sailor.databinding.FragmentExploreBinding;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -25,6 +33,28 @@ public class ExploreFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentExploreBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.googleMap);
+
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(@NonNull @NotNull LatLng latLng) {
+                        //When clicked on map
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.position(latLng);
+                        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+                        googleMap.clear();
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                latLng,10
+                        ));
+                        googleMap.addMarker(markerOptions);
+                    }
+                });
+            }
+        });
         return root;
     }
 
