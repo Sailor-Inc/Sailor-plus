1ad8961903a64cbc6109333a6585a0f09e5e61ab
Carlo Angel Luján García <carlouxljlj@gmail.com>
2021-07-24 17:14:03 -0500

    Added a button in the explore fragment

--- Sailor/app/src/main/java/com/carlolj/sailor/ui/explore/PinFragment.java
+++ Sailor/app/src/main/java/com/carlolj/sailor/ui/explore/PinFragment.java
@@ -97,7 +97,12 @@ public class PinFragment extends Fragment {
                     Log.e("PinActivity", "Issue with getting locations", e);
                 }
                 if (receivedPosts != null) {
-                    Collections.sort(receivedPosts, (Post a1, Post a2) -> a1.getTopsNumber()+a2.getTopsNumber());
+                    // Before Java 8, sorting a collection would involve creating an anonymous inner class for the
+                    // Comparator used in the sort, with the introduction of lambdas, we can bypass the
+                    // anonymous inner class and achieve the same result with simple, functional semantics,
+                    // and we can further simplify the expression by not specifying the type definitions
+                    // – the compiler is capable of inferring these on its own (see a1 and a2 objects doesn't have a type):
+                    Collections.sort(receivedPosts, (a1, a2) -> a2.getTopsNumber()-a1.getTopsNumber());
                     int maxLength = receivedPosts.size();
                     if (maxLength > 50) {
                         allPosts.addAll(receivedPosts.subList(0,50));
@@ -110,60 +115,5 @@ public class PinFragment extends Fragment {
                 }
             }
         });
-    }
-
-    public LinkedHashMap<Post, Integer> sortHashMapAscendingOld(
-            HashMap<Post, Integer> passedMap) {
-        List<Post> mapKeys = new ArrayList<>(passedMap.keySet());
-        List<Integer> mapValues = new ArrayList<>(passedMap.values());
-        Collections.sort(mapValues);
-        //Collections.sort(mapKeys);
-
-        LinkedHashMap<Post, Integer> sortedMap =
-                new LinkedHashMap<>();
-
-        Iterator<Integer> valueIt = mapValues.iterator();
-        while (valueIt.hasNext()) {
-            Integer val = valueIt.next();
-            Iterator<Post> keyIt = mapKeys.iterator();
-
-            while (keyIt.hasNext()) {
-                Post key = keyIt.next();
-                Integer comp1 = passedMap.get(key);
-                Integer comp2 = val;
-
-                if (comp1.equals(comp2)) {
-                    keyIt.remove();
-                    sortedMap.put(key, val);
-                    break;
-                }
-            }
-        }
-        return sortedMap;
-    }
-
-    public LinkedHashMap<Post, Integer> sortHashMapDescending(
-            HashMap<Post, Integer> unSortedMap){
-        LinkedHashMap<Post, Integer> reverseSortedMap = new LinkedHashMap<>();
-
-        //Use Comparator.reverseOrder() for reverse ordering
-        unSortedMap.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
-
-        return reverseSortedMap;
-    }
-
-    public LinkedHashMap<Post, Integer> sortHashMapAscendingNew(
-            HashMap<Post, Integer> unSortedMap){
-        LinkedHashMap<Post, Integer> sortedMap = new LinkedHashMap<>();
-
-        unSortedMap.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue())
-                .forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
-
-        return sortedMap;
     }
 }
